2156f5c04081d89643233ce315af068c9b402746
alonecandies <dragonfrezze@gmail.com>
2024-08-28 16:40:17 +0700

    chore: clear msg test time 1

--- src/index.js
+++ src/index.js
@@ -225,8 +225,8 @@ HttpProvider.prototype.send = function (payload, callback) {
           callback(null, data);
         }
       })
-      .catch(function () {
-        callback({ message: errors.InvalidResponse(response).message, host: host });
+      .catch(function (e) {
+        callback({ message: e, host: host });
       });
   };
 
